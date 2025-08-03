17687e2fdf6eca3550dae9496d2a76a805fca07f
ARTariqDev <tariqmahmood.dmp@gmail.com>
2025-08-03 05:09:52 +0500

    .

--- app/routes/layout.jsx
+++ app/routes/layout.jsx
@@ -7,7 +7,6 @@ export default function layout() {
   return (
     <main>
       <HeroBg />
-      {/*Fixed this overlay that was causing issues with the bg (it was a really opaque gradient bg before so i changed it to a translucent dark blue */}
       <div className="fixed inset-0 w-full h-full bg-[#01091f]/96 z-[-1]" />
       <Navbar />
       <Outlet />
