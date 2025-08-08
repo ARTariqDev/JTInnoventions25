822474e5c0d5f4977f6ac370d216ab97658c4ba4
Farjad <farjadabbas12@gmail.com>
2025-08-08 22:13:56 +0500

    Fixed the bg problem (hopefully)

--- app/components/HeroBg.jsx
+++ app/components/HeroBg.jsx
@@ -2,31 +2,31 @@ function HeroBg() {
   return (
     <picture>
       <source
-        srcSet="app/assets/heroImg/hero@500w.jpg 1x, app/assets/heroImg/hero@914w.jpg 2x"
+        srcSet="/app/assets/heroImg/hero@500w.jpg 1x, /app/assets/heroImg/hero@914w.jpg 2x"
         media="(max-width: 500px)"
       />
       <source
-        srcSet="app/assets/heroImg/hero@914w.jpg 1x, app/assets/heroImg/hero@2000w.jpg 2x"
+        srcSet="/app/assets/heroImg/hero@914w.jpg 1x, /app/assets/heroImg/hero@2000w.jpg 2x"
         media="(max-width: 914px)"
       />
       <source
-        srcSet="app/assets/heroImg/hero@1313w.jpg 1x, app/assets/heroImg/hero@2000w.jpg 2x"
+        srcSet="/app/assets/heroImg/hero@1313w.jpg 1x, /app/assets/heroImg/hero@2000w.jpg 2x"
         media="(max-width: 1313px)"
       />
       <source
-        srcSet="app/assets/heroImg/hero@1500w.jpg 1x, app/assets/heroImg/hero.jpg 2x"
+        srcSet="/app/assets/heroImg/hero@1500w.jpg 1x, /app/assets/heroImg/hero.jpg 2x"
         media="(max-width: 1500px)"
       />
       <source
-        srcSet="app/assets/heroImg/hero@2000w.jpg 1x, app/assets/heroImg/hero.jpg 2x"
+        srcSet="/app/assets/heroImg/hero@2000w.jpg 1x, /app/assets/heroImg/hero.jpg 2x"
         media="(max-width: 2000px)"
       />
       <source
-        srcSet="app/assets/heroImg/hero.jpg"
+        srcSet="/app/assets/heroImg/hero.jpg"
         media="(min-width: 2001px)"
       />
       <img
-        src="app/assets/heroImg/hero.jpg"
+        src="/app/assets/heroImg/hero.jpg"
         alt="Hero background"
         className="fixed inset-0 w-full h-full object-cover object-center z-[-2]"
       />
