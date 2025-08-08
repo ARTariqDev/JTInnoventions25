051734e408de8003b81b09d43c006385d887c072
Farjad <farjadabbas12@gmail.com>
2025-08-08 22:28:32 +0500

    .

--- app/components/HeroBg.jsx
+++ app/components/HeroBg.jsx
@@ -2,28 +2,31 @@ function HeroBg() {
   return (
     <picture>
       <source
-        srcSet="../assets/heroImg/hero@500w.jpg 1x, ../assets/heroImg/hero@914w.jpg 2x"
+        srcSet="/app/public/heroImg/hero@500w.jpg 1x, /app/public/heroImg/hero@914w.jpg 2x"
         media="(max-width: 500px)"
       />
       <source
-        srcSet="../assets/heroImg/hero@914w.jpg 1x, ../assets/heroImg/hero@2000w.jpg 2x"
+        srcSet="/app/public/heroImg/hero@914w.jpg 1x, /app/public/heroImg/hero@2000w.jpg 2x"
         media="(max-width: 914px)"
       />
       <source
-        srcSet="../assets/heroImg/hero@1313w.jpg 1x, ../assets/heroImg/hero@2000w.jpg 2x"
+        srcSet="/app/public/heroImg/hero@1313w.jpg 1x, /app/public/heroImg/hero@2000w.jpg 2x"
         media="(max-width: 1313px)"
       />
       <source
-        srcSet="../assets/heroImg/hero@1500w.jpg 1x, ../assets/heroImg/hero.jpg 2x"
+        srcSet="/app/public/heroImg/hero@1500w.jpg 1x, /app/public/heroImg/hero.jpg 2x"
         media="(max-width: 1500px)"
       />
       <source
-        srcSet="../assets/heroImg/hero@2000w.jpg 1x, ../assets/heroImg/hero.jpg 2x"
+        srcSet="/app/public/heroImg/hero@2000w.jpg 1x, /app/public/heroImg/hero.jpg 2x"
         media="(max-width: 2000px)"
       />
-      <source srcSet="../assets/heroImg/hero.jpg" media="(min-width: 2001px)" />
+      <source
+        srcSet="/app/public/heroImg/hero.jpg"
+        media="(min-width: 2001px)"
+      />
       <img
-        src="../assets/heroImg/hero.jpg"
+        src="/app/public/heroImg/hero.jpg"
         alt="Hero background"
         className="fixed inset-0 w-full h-full object-cover object-center z-[-2]"
       />
